29a2faac558a62cf7322f34047d8a0ca35c73197
SimonHsiao-NCO <Simon.Hsiao@noaa.gov>
2022-06-23 16:34:34 +0000

    WCOSS2 RFC 9628 cleanup.v1.0.5 update commit

--- scripts/exrzdm_cleanup.sh
+++ scripts/exrzdm_cleanup.sh
@@ -57,7 +57,67 @@ then
              err_exit
            fi
            done # for site loop
-         fi # end if cleanup_com equals yes
+        fi
+      elif echo $clean_list| grep -q hysplit
+        #HYSPLIT CLEANUP
+      then
+        NET=""
+        directory=`echo $clean_list | awk -F"|" '{print "/home/www/"$1"/"$2""}'`
+        prod_keep00=`echo $clean_list | awk -F"|" '{print $3}'`
+        prod_keep12=`echo $clean_list | awk -F"|" '{print $4}'`
+        para_keep00=`echo $clean_list | awk -F"|" '{print $5}'`
+        para_keep12=`echo $clean_list | awk -F"|" '{print $6}'`
+        test_keep00=`echo $clean_list | awk -F"|" '{print $7}'`
+        test_keep12=`echo $clean_list | awk -F"|" '{print $8}'`
+        sdm_keep00=`echo $clean_list | awk -F"|" '{print $9}'`
+        sdm_keep12=`echo $clean_list | awk -F"|" '{print $10}'`
+        if [ ${cleanup_com:?} == YES ]; then
+           com=`eval echo ${directory/\${envir}/${clean_envir}}`
+           keep=`eval echo "$"${clean_envir}_keep${cyc}`
+           if test `eval echo $clean_envir` = "prod" -o `eval echo $clean_envir` = "para"
+           then
+           NET=`echo $clean_list | awk -F"|" '{print $1}'`
+             for site in $SITELIST
+             do
+               let numattempts=3
+               while [ $numattempts -gt 0 ]; do
+                 date
+                 timeout ${MAXTIME} ssh nwprod@${site}  "date; hostname; export PDYp1=$PDYp1 cyc=$cyc;  /home/people/nco/nwprod/bin/cleanup_rmdir_rzdm.sh $PDY ${com/prod/} $keep $cyc $NET 2>&1"
+                 err=$?
+                 if [ $err -eq 124 ]; then
+                   ((numattempts--))
+                   sleep 5
+                 else
+                   let numattempts=-1  # successful completion on ssh
+                 fi
+               done # for numattempts loop
+               if [ $numattempts -eq 0 ]; then
+                 echo "FATAL ERROR: ssh failed after three attempts"
+                 err_exit
+               fi
+             done # for site loop
+           else
+             for site in $SITELIST
+             do
+               let numattempts=3
+               while [ $numattempts -gt 0 ]; do
+                 date
+                 timeout ${MAXTIME} ssh nwprod@${site}  "date; hostname; export PDYp1=$PDYp1 cyc=$cyc;  /home/people/nco/nwprod/bin/cleanup_rmdir_rzdm.sh $PDY ${com/prod/} $keep $cyc 2>&1"
+                 err=$?
+                 if [ $err -eq 124 ];  then
+                   ((numattempts--))
+                   sleep 5
+                 else
+                   let numattempts=-1  # successful completion on ssh
+                 fi
+               done # for numattempts loop
+               if [ $numattempts -eq 0 ]; then
+                 echo "FATAL ERROR: ssh failed after three attempts"
+                 err_exit
+               fi
+             done # for site loop
+           fi
+        fi
       else
         echo "NET/runid not set "
         exit 1
